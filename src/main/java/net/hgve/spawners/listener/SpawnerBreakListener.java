9b55a0cd6662df5130c94d8d6be4757364d6309a
GrijsjePlay <113257202+GrijsjePlay@users.noreply.github.com>
2024-12-25 00:25:01 +0100

    SpawnerBreakListener update

--- src/main/java/net/hgve/spawners/listener/SpawnerBreakListener.java
+++ src/main/java/net/hgve/spawners/listener/SpawnerBreakListener.java
@@ -1,6 +1,36 @@
 package net.hgve.spawners.listener;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class SpawnerBreakListener implements Listener {
+
+    @EventHandler
+    public void onSpawnerBreak(BlockBreakEvent event) {
+
+        if (!(event.getBlock().getType() == Material.SPAWNER)) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+
+        if (!(player.hasPermission("spawners.silktouch"))) {
+            return;
+        }
+
+        ItemStack tool = player.getInventory().getItemInMainHand();
+
+        if (!(tool.containsEnchantment(Enchantment.SILK_TOUCH))) {
+            return;
+        }
+
+        // spawner mob storing
+
+    }
+
 }
