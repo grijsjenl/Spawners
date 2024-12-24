2eb5a1369b71cb35047c45abe0bc1dd260d10f6c
GrijsjePlay <113257202+GrijsjePlay@users.noreply.github.com>
2024-12-24 20:54:43 +0100

    Initial commit

---
+++ src/main/java/net/hgve/spawners/Spawners.java
@@ -0,0 +1,17 @@
+package net.hgve.spawners;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spawners extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/net/hgve/spawners/listener/spawnerBreakListener.java
@@ -0,0 +1,4 @@
+package net.hgve.spawners.listener;
+
+public class spawnerBreakListener {
+}
