bf54b670847528b380af6a32624505db4f54dc60
GrijsjePlay <113257202+GrijsjePlay@users.noreply.github.com>
2025-01-09 21:54:58 +0100

    Start of spawner dropping system

--- src/main/java/net/hgve/spawners/listener/SpawnerBreakListener.java
+++ src/main/java/net/hgve/spawners/listener/SpawnerBreakListener.java
@@ -1,5 +1,6 @@
 package net.hgve.spawners.listener;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.CreatureSpawner;
 import org.bukkit.enchantments.Enchantment;
@@ -9,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class SpawnerBreakListener implements Listener {
 
@@ -37,8 +39,13 @@ public class SpawnerBreakListener implements Listener {
         CreatureSpawner spawner = (CreatureSpawner) event.getBlock();
 
         EntityType spawnerEntity = spawner.getSpawnedType();
+        ItemStack spawnerItem = new ItemStack(Material.SPAWNER);
 
+        // maybe a better way?
+        event.setCancelled(true);
+        event.getBlock().setType(Material.AIR);
 
+        // way to drop spawner as item
 
     }
 
--- src/main/java/net/hgve/spawners/listener/SpawnerPlaceListener.java
+++ src/main/java/net/hgve/spawners/listener/SpawnerPlaceListener.java
@@ -5,7 +5,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class SpawnerPlaceListener implements Listener {
 
